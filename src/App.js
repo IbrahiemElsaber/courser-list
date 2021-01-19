82ea501e4f231b0ca529e41455adcd50b50a6743
IbrahiemElsaber <ebrahimelsaber@hotmail.com>
2021-01-19 13:36:55 +0200

    adding delete function and handel it to delete depending on course index

--- src/App.js
+++ src/App.js
@@ -6,32 +6,46 @@ import CourseList from "./components/course.list.component";
 
 class App extends Component {
 	state = {
+		// courses: [],
 		courses: [{ courseName: "HTML" }, { courseName: "Js" }, { courseName: "CSS" }],
 		current: "",
 	};
 
+	//handle update course
 	handleCourseUpdate = e => {
 		this.setState({
 			current: e.target.value,
 		});
 	};
 
+	//handle add course
 	handleAddCourse = e => {
 		e.preventDefault();
 		let currentCourse = this.state.current;
 		let courses = this.state.courses;
-		courses.push({ courseName: currentCourse });
+		courses.push({ courseName: currentCourse }); //update courses array after adding new course object
 		this.setState({
 			courses,
 			current: "" /*set current to empty string to handle input value to equal empty string after submit */,
 		});
 	};
+
+	//handle delete course
+	handleDeleteCourse = index => {
+		console.log(`index= ${index}`);
+		let courses = this.state.courses;
+		courses.splice(index, 1);
+		this.setState({
+			courses,
+		});
+	};
+
 	render() {
 		const { courses } = this.state;
 		const coursesList = courses.map((course, index) => {
-			// console.log(`course: ${course.courseName}`);
-			return <CourseList key={Math.random() * 5} courseDetails={course} />;
+			return <CourseList courseDetails={course} key={index} index={index} deleteCourse={this.handleDeleteCourse} />;
 		});
+
 		return (
 			<div className='container'>
 				<h1>App Component</h1>
--- src/components/course.list.component.js
+++ src/components/course.list.component.js
@@ -3,10 +3,17 @@ import React, { Component, Fragment } from "react";
 class CourseList extends Component {
 	render() {
 		// console.log(`this.props = ${this.props.courseDetails.courseName}`);
-		// console.log(this.props);
+		console.log(`CourseList props index = ${this.props.index}`);
+
 		return (
 			<Fragment>
-				<li>{this.props.courseDetails.courseName}</li>
+				<li className='row'>
+					<span className='m-2'>{this.props.courseDetails.courseName}</span>
+					<button className='btn-danger m-2 w-25' onClick={this.props.deleteCourse}>
+						Delete
+					</button>
+					<button className='btn-warning m-2 w-25'>Update</button>
+				</li>
 			</Fragment>
 		);
 	}
