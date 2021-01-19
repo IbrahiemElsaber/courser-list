2e021b944e8d806cf9cada5b777e2c2896075357
IbrahiemElsaber <ebrahimelsaber@hotmail.com>
2021-01-19 11:28:00 +0200

    setupd addCourse and updateCourse handlers functions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 // import React from "react";
 import React, { Component } from "react";
+// import "./App.css";
 import CourseForm from "./components/course.form.component";
 import CourseList from "./components/course.list.component";
 
 class App extends Component {
 	state = {
 		courses: [{ courseName: "HTML" }, { courseName: "Js" }, { courseName: "CSS" }],
+		current: "",
 	};
 
+	handleCourseUpdate = e => {
+		// console.log("input course =  ", e.target.value);
+		this.setState({
+			current: e.target.value,
+		});
+	};
+	handleAddCourse = e => {
+		e.preventDefault();
+		console.log(` current: ${this.state.current}`);
+		// alert(this.state.current);
+	};
 	render() {
 		const { courses } = this.state;
 		const coursesList = courses.map((course, index) => {
-			console.log(`course: ${course.courseName}`);
+			// console.log(`course: ${course.courseName}`);
 			return <CourseList key={Math.random() * 5} courseDetails={course} />;
 		});
 		return (
-			<div className='App'>
+			<div className='container'>
 				<h1>App Component</h1>
-				<hr />
-				<CourseForm />
-				<hr />
+
+				<CourseForm updateCourse={this.handleCourseUpdate} submitCourseValue={this.handleAddCourse} />
 
 				<div>
 					Courses names are : <ul>{coursesList}</ul>
--- src/components/course.form.component.js
+++ src/components/course.form.component.js
@@ -1,6 +1,11 @@
 import React from "react";
 
-const CourseForm = () => {
-	return <div>form</div>;
+const CourseForm = props => {
+	return (
+		<form onSubmit={props.submitCourseValue}>
+			<input type='text' onChange={props.updateCourse} />
+			<input type='submit' value='submit' />
+		</form>
+	);
 };
 export default CourseForm;
